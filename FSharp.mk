f259970a057a1c0360a43054f6bf19a828e58cfd
joranvar <bart.post@gmail.com>
2015-10-19 11:27:56 +0200

    Remove dependency and just symlink FSharp.Core.dll
    This prevents the dll from being included in the FSC call.  Fixes #3.

--- FSharp.mk
+++ FSharp.mk
@@ -1,6 +1,9 @@
 # Define the compiler location here
 FSC ?= env fsharpc
 
+# Define the location of FSharp.Core.dll here
+FSharp.Core.dll ?= /nix/store/9nvx5380w2md40yzr63hbyh22aafsw4j-fsharp-3.1.2.5/lib/mono/4.5/FSharp.Core.dll
+
 # Phony targets: ignore files with those names
 .PHONY: fsharp
 
@@ -8,10 +11,9 @@ fsharp: $(ASSEMBLIES)
 
 # FSharp assembly template
 define FSHARP_template =
- $(1): $(dir $(1))FSharp.Core.dll
-
  $(1): $$($(1)_sources)
 	$$(FSC) -o $$@ $$<
+	ln -s $$(FSharp.Core.dll) $(dir $(1))
 endef
 
 $(foreach assembly,$(ASSEMBLIES),$(eval $(call FSHARP_template,$(assembly))))
